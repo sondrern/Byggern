1a8a602940302380e1752c618a0804bcd6357d94
sondrern <sondrern@gmail.com>
2016-10-04 17:09:59 +0200

    Add files via upload

--- Lab6/MCP2515.c
+++ Lab6/MCP2515.c
@@ -33,22 +33,37 @@ void mcp2515_write(uint8_t value, uint8_t addr){
     SPI_enable(0);
 }
 
-void mcp2515_request_to_send(){
+void mcp2515_request_to_send(void){
+
+   	SPI_enable(1);
+    SPI_send(MCP_RTS_ALL);
+    SPI_enable(0)
 
 }
 
 void mcp2515_bit_modify(uint8_t addr, uint8_t mask, uint8_t val){
     SPI_enable(1);
 
-    spi_write(MCP_BITMOD);
-    spi_write(addr);
-    spi_write(mask);
-    spi_write(val);
+    SPI_send(MCP_BITMOD);
+    SPI_send(addr);
+    SPI_send(mask);
+    SPI_send(val);
 
     SPI_enable(0);
 }
 
 
+uint8_t mcp2515_read_status(void){
+	SPI_enable(1);
+    SPI_send(MCP_READ_STATUS);
+    uint8_t value = SPI_receive();
+    SPI_enable(0);
+    return value;
+}
 
 
-mcp2515_bit_modify(MCP_CANCTRL, 0xE0, 0x2);
+void mcp2515_reset(void){
+	SPI_enable(1);
+    SPI_send(MCP_RESET);
+    SPI_enable(0);
+}
--- Lab6/MCP2515.h
+++ Lab6/MCP2515.h
@@ -12,9 +12,23 @@
 #define MCP_CANCTRL     0x0F
 #define MCP_BITMOD      0x05
 
+#define MCP_RESET       0xC0
+#define MCP_READ_STATUS 0xA0
+
+#define MCP_RTS_TX0     0x81
+#define MCP_RTS_TX1     0x82
+#define MCP_RTS_TX2     0x84
+#define MCP_RTS_ALL     0x87
+
 
 uint8_t mcp2515_read(uint8_t addr);
 
 void mcp2515_write(uint8_t value, uint8_t addr);
 
-void mcp2515_request_to_send();
+void mcp2515_request_to_send(void);
+
+void mcp2515_bit_modify(uint8_t addr, uint8_t mask, uint8_t val);
+
+uint8_t mcp2515_read_status(void);
+
+void mcp2515_reset(void);
