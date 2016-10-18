d8e20dc780151e29d9196317338127bdc5824e7e
sondrern <sondrern@gmail.com>
2016-10-18 10:58:55 +0200

    Update spic.c

--- lab7/spic.c
+++ lab7/spic.c
@@ -1,13 +1,21 @@
 #include "spi.h""
 #include <avr/pgmspace.h>
 
+#define atmega162
+//#define atmega2560
+
 void SPI_init(void){
 
-	
+	#ifdef atmega162
 	DDRB |= (1<<DDB7) | (1<<DDB5) | (1<<DDB4) ;
 	DDRB &=~(1<<DDB6);
 	PORTB |= (1<<DDB6);
+	#else
+	DDRB |= (1<<DDB1) | (1<<DDB2) | (1<<DDB0) ;
+	DDRB &=~(1<<DDB3);
+	PORTB |= (1<<DDB3);
 	
+	#endif
 	
 	
 	
@@ -37,11 +45,18 @@ char SPI_receive(void){
 }
 
 void SPI_enable(uint8_t enable){
+	#ifdef atmega162
 	if(enable){
 		(PORTB &= ~(1<<PB4));
 	}
 	else{
 		(PORTB |= (1<<PB4));
 	}
-
+	#else
+	if(enable){
+		(PORTB &= ~(1<<PB0));
+	}
+	else{
+		(PORTB |= (1<<PB0));
+	}
 }
